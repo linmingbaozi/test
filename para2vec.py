f67f24609b082c3c17af8a18fb9816ba195c3e72
linmingbaozi <34297753+linmingbaozi@users.noreply.github.com>
2017-12-19 10:39:09 +0800

    Add files via upload

---
+++ para2vec.py
@@ -0,0 +1,31 @@
+from gensim.models import doc2vec
+from collections import namedtuple
+import string
+import numpy as np
+
+txt = ['./train_img_ann_cat.txt', 'val_img_ann_cat.txt']
+save = ['./para2vec_train.npy', './para2vec_val.npy']
+
+for j in range(len(txt)):
+    txt_path = txt[j]
+
+    docs = open(txt_path, 'r').readlines()
+
+    for i in range(len(docs)):
+        docs[i] = docs[i].split('\t')[1]
+
+    anns = []
+
+    analyzedDocument = namedtuple('AnalyzedDocument', 'words tags')
+
+    for i, text in enumerate(docs):
+        delset = string.punctuation
+        text = text.translate(None, delset)
+        words = text.lower().split()
+        tags = [i]
+        anns.append(analyzedDocument(words, tags))
+
+    model =doc2vec.Doc2Vec(anns, size = 100, window = 300, min_count = 1, workers = 4)
+
+    para2vec = np.array(model.docvecs)
+    np.save(save[j], para2vec)
